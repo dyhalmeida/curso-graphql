fd3d214d847d9165d5d52a020b44d2b6e838ada9
Diego Almeida <dyhalmeida@gmail.com>
2022-01-30 20:40:39 -0300

    feat: add scalar type

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const { ApolloServer, gql } = require('apollo-server')
 
 const typeDefs = gql`
+
+  scalar Date
+
   # Pontos de entradas da API
   type Query {
     now: String!
+    otherNow: Date!
   }
 `
 
 const resolvers = {
   Query: {
     now: () => {
-      
+
       const now = new Date()
       const hour = now.getHours().toString().length === 1 ? `0${now.getHours()}` : `${now.getHours()}`
       const minute = now.getMinutes().toString().length === 1 ? `0${now.getMinutes()}` : `${now.getMinutes()}`
       const second = now.getSeconds().toString().length === 1 ? `0${now.getSeconds()}` : `${now.getSeconds()}`
 
       return `${hour}:${minute}:${second}`
+    },
+    otherNow: () => {
+      return new Date()
     }
   }
 }
