8252475d86cb5e24ff6fbdf080e11e8dca3bebd1
Diego Almeida <dyhalmeida@gmail.com>
2022-03-09 20:11:54 -0300

    feat: add query and mutation user

--- resolvers/Mutation/user.js
+++ resolvers/Mutation/user.js
@@ -2,9 +2,33 @@ const knex = require('../../config/db')
 
 module.exports = {
   createUser: async (_, { data }) => {
+    try {
+      const [id] = await knex.insert(data).into('users')
+      const user = await knex.select('*').from('users').where({ id }).first()
+      return user
+    } catch (error) {
+      if (error.sqlMessage) {
+        throw new Error(error.sqlMessage)
+      }
+      throw new Error('Internal server error')
+    } 
   },
   deleteUser: async (_, { filters }) => {
+    try {
+      const userFound = await knex.select('*').from('users').where({ id: filters.id }).first()
+      if (!userFound) throw new Error('User not found')
+      await knex.delete().from('users').where({ id: filters.id })
+      return userFound
+    } catch (error) {
+      if (error.sqlMessage || error.message) {
+        throw new Error(error.sqlMessage || error.message)
+      }
+      throw new Error('Internal server error')
+    } 
   },
   updateUser: async (_, { filters, data }) => {
+    await knex('users').update({ ...data }).where({ id: filters.id })
+    const user = await knex.select('*').from('users').where({ id: filters.id }).first()
+    return user
   }
 }
--- resolvers/Query/user.js
+++ resolvers/Query/user.js
@@ -2,7 +2,20 @@ const knex = require('../../config/db')
 
 module.exports = {
   users: async () => {
+    try {
+      const users = await knex.select('*').from('users')
+        .then(users => users.map(user => ({...user})))
+      return users
+    } catch (error) {
+      throw new Error("Internal server error")  
+    }
   },
   user: async (_, { filter }) => {
+    try {
+      const user = await knex.select('*').from('users').where({ id: filter.id }).first()
+      return user
+    } catch (error) {
+      throw new Error("Internal server error")  
+    }
   }
 }
