9a06e4e51e232fd9a5013bd87bb46bb810fb1a23
Diego Almeida <dyhalmeida@gmail.com>
2022-03-09 20:12:10 -0300

    feat: add query and mutation profile

--- resolvers/Mutation/profile.js
+++ resolvers/Mutation/profile.js
@@ -2,9 +2,30 @@ const knex = require('../../config/db')
 
 module.exports = {
   createProfile: async (_, { data }) => {
+    try {
+      const [id] = await knex.insert(data).into('profiles')
+      const profile = await knex.select('*').from('profiles').where({ id }).first()
+      return profile
+    } catch (error) {
+      throw new Error(error.sqlMessage)
+    }
   },
   deleteProfile: async (_, { filters }) => {
+    try {
+      const profileFound = await knex.select('*').from('profiles').where({ id: filters.id }).first()
+      if (!profileFound) throw new Error('Profile not found')
+      await knex.delete().from('profiles').where({ id: filters.id })
+      return profileFound
+    } catch (error) {
+      if (error.sqlMessage || error.message) {
+        throw new Error(error.sqlMessage || error.message)
+      }
+      throw new Error('Internal server error')
+    } 
   },
   updateProfile: async (_, { filters, data }) => {
+    await knex('profiles').update({ ...data }).where({ id: filters.id })
+    const profile = await knex.select('*').from('profiles').where({ id: filters.id }).first()
+    return profile
   }
 }
--- resolvers/Query/profile.js
+++ resolvers/Query/profile.js
@@ -2,7 +2,20 @@ const knex = require('../../config/db')
 
 module.exports = {
   profiles: async () => {
+    try {
+      const profiles = await knex.select('*').from('profiles')
+        .then(profiles => profiles.map(profile => ({...profile})))
+      return profiles
+    } catch (error) {
+      throw new Error("Internal server error")  
+    }
   },
   profile: async (_, { filter }) => {
+    try {
+      const profile = await knex.select('*').from('profiles').where({ id: filter.id }).first()
+      return profile
+    } catch (error) {
+      throw new Error("Internal server error")  
+    }
   }
 }
