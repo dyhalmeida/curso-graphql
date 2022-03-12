d6b85c4d46f525a1cb51ca85c20d583b1280beae
Diego Almeida <dyhalmeida@gmail.com>
2022-03-12 09:32:23 -0300

    refactor: query profile

--- resolvers/Query/profile.js
+++ resolvers/Query/profile.js
@@ -3,17 +3,14 @@ const knex = require('../../config/db')
 module.exports = {
   profiles: async () => {
     try {
-      const profiles = await knex.select('*').from('profiles')
-        .then(profiles => profiles.map(profile => ({...profile})))
-      return profiles
+      return await knex.select('*').from('profiles')
     } catch (error) {
       throw new Error("Internal server error")  
     }
   },
   profile: async (_, { filter }) => {
     try {
-      const profile = await knex.select('*').from('profiles').where({ id: filter.id }).first()
-      return profile
+      return await knex.select('*').from('profiles').where({ id: filter.id }).first()
     } catch (error) {
       throw new Error("Internal server error")  
     }
