e2d43ed9d9cbb05b6c20b2f7b1015dbd4438bd33
Diego Almeida <dyhalmeida@gmail.com>
2022-03-18 20:56:13 -0300

    feat: add env

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
+require('dotenv').config()
 const { ApolloServer } = require('apollo-server')
 const { importSchema } = require('graphql-import')
 const resolvers = require('./resolvers')
 const typeDefs = importSchema('./schema/index.graphql')
-
-const { port } = require('./.env')
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
 })
 
-server.listen(port).then(({ url }) => {
+server.listen(process.env.APP_SERVER_PORT).then(({ url }) => {
   console.log(`Server is runnig on ${url}`)
 })
--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,14 @@
-const { connection } = require('./.env')
+require('dotenv').config()
 
 module.exports = {
   client: 'mysql',
-  connection,
+  connection: {
+    host: process.env.APP_DB_HOST,
+    port: process.env.APP_DB_PORT,
+    database: process.env.APP_DB_NAME,
+    user: process.env.APP_DB_USER,
+    password: process.env.APP_DB_PASSWORD
+  },
   pool: {
     min: 2,
     max: 10
